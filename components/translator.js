85d37903f89c22f41d904cb0f1cac4f6705167f3
abdsa <abd.sab111@gmail.com>
2022-03-04 21:10:45 +0300

    This is first commit

--- components/translator.js
+++ components/translator.js
@@ -2,9 +2,86 @@ const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
+function Translator() {
 
-class Translator {
+  this.americanOnlyTranslator = function(input) {
+    let splitted = input.split(" ");
+    for (let i = 0; i < splitted.length; i++) {
+      if (/\d\d\:\d\d/.test(splitted[i])) {
+        let matched = splitted[i].split(":").join(".")
+        splitted.splice(i, 1, `<span class="highlight">${matched}</span>`)
+        splitted = splitted.join(" ")
+      }
+      else if (americanOnly[`${splitted[i]}`]) {
+        console.log("only")
+        splitted.splice(i, 1, `<span class="highlight">${americanOnly[`${splitted[i]}`]}</span>`)
+        splitted = splitted.join(" ");
+      }
+      else if (americanToBritishSpelling[`${splitted[i]}`]) {
+        console.log("spelling")
+        splitted.splice(i, 1, `<span class="highlight">${americanToBritishSpelling[`${splitted[i]}`]}</span>`)
+        splitted = splitted.join(" ");
+        consol
+      }
+      else if (americanToBritishTitles[`${splitted[i]}`]) {
+        console.log("titles")
+        splitted.splice(i, 1, `<span class="highlight">${americanToBritishTitles[`${splitted[i]}`]}</span>`)
+        splitted = splitted.join(" ");
+      }
+      else if (americanOnly[`${splitted[i]} ${splitted[i + 1]}`]) {
+        console.log("more than a word")
+        splitted.splice(i, 2, `<span class="highlight">${americanOnly[`${splitted[i]} ${splitted[i + 1]}`]}</span>`)
+        splitted = splitted.join(" ");
+      }
+      if(i === splitted.length - 1){
+        if(splitted === input.split(" ")){
+          splitted = undefined;
+          return splitted;
+        } else {
+          return splitted;
+        }
+      }
+    }
+    
+  }
 
+  this.britishOnlyTranslator = function(input) {
+    let britishToAmericanSpelling = Object.entries(americanToBritishSpelling).reduce((acc, [key, value]) => (acc[value] = key, acc), {})
+    let britishToAmericanTitles = Object.entries(americanToBritishTitles).reduce((acc, [key, value]) => (acc[value] = key, acc), {})
+
+
+    let splitted = input.split(" ");
+    for (let i = 0; i < splitted.length; i++) {
+      if (/\d\d\.\d\d/.test(splitted[i])) {
+        console.log("time")
+        let matched = splitted[i].split(".").join(":")
+        splitted.splice(i, 1, `<span class="highlight">${matched}</span>`)
+        splitted = splitted.join(" ")
+      }
+      else if (britishOnly[`${splitted[i]}`]) {
+        console.log("only")
+        splitted.splice(i, 1, `<span class="highlight">${britishOnly[`${splitted[i]}`]}</span>`)
+        splitted = splitted.join(" ");
+      }
+      else if (britishToAmericanSpelling[`${splitted[i]}`]) {
+        console.log("spelling")
+        splitted.splice(i, 1, `<span class="highlight">${britishToAmericanSpelling[`${splitted[i]}`]}</span>`)
+        splitted = splitted.join(" ");
+      }
+      else if (britishToAmericanTitles[`${splitted[i]}`]) {
+        console.log("titles")
+        splitted.splice(i, 1, `<span class="highlight">${britishToAmericanTitles[`${splitted[i]}`]}</span>`)
+        splitted = splitted.join(" ");
+      } else if (britishOnly[`${splitted[i]} ${splitted[i + 1]}`]) {
+        console.log("more than a word")
+        splitted.splice(i, 2, `<span class="highlight">${britishOnly[`${splitted[i]} ${splitted[i + 1]}`]}</span>`);
+        splitted = splitted.join(" ");
+      }
+    }
+    if(splitted === input.split(" ")){
+      splitted = undefined;
+      return splitted
+    }
+  }
 }
-
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,45 @@
 
 const Translator = require('../components/translator.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
       
+      if (req.body.text === "") {
+        res.json({ error: 'No text to translate' })
+        return;
+      }
+      if (!req.body.text || !req.body.locale) {
+        res.json({ error: 'Required field(s) missing' })
+        return;
+      }
+
+
+      let result;
+      if (req.body.locale === "american-to-british") {
+        result = translator.americanOnlyTranslator(req.body.text)
+        if (result === undefined) {
+          res.json({ translation: "Everything looks good to me!" })
+          return;
+        }
+        console.log(result)
+
+        res.json({ text: req.body.text, translation: result })
+        return;
+      }
+      else if (req.body.locale === "british-to-american") {
+        result = translator.britishOnlyTranslator(req.body.text)
+        if (result === undefined) {
+          res.json({ translation: "Everything looks good to me!" })
+          return;
+        }
+        console.log(result)
+        res.json({ text: req.body.text, translation: result })
+        return;
+      }
+
     });
 };
